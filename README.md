605e78615d5e55dfedc9f0e5a3dcb8305a25451f
Marcus Bynoe <marcusbynoe57@gmail.com>
2023-01-10 18:17:17 -0700

    completed lab 2

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 - Start time: 3:45pm (Mountain Time)
 
-- Finish time: 
+- Finish time: 6:14pm (Mountain Time)
 
-- Actual time needed to complete: 
+- Actual time needed to complete: 2 hours and 49 minutes (It sucks doing CSS in bootstrap)
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 
 class HornedBeast extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       favorites: 0,
@@ -17,16 +18,29 @@ class HornedBeast extends React.Component {
 
   render() {
     return (
-    <>
-      <article>
+
+      <>
+
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" onClick={this.handleFav} src={this.props.image_url} alt={this.props.title} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>💖{this.state.favorites} Favorites</Card.Text>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        {/* <article>
       <h2>{this.props.title}</h2>
       <p> 💖{this.state.favorites} Favorites</p>
-      <p onClick={this.handleFav}>Add to favorites!</p>
-      <img src={this.props.image_url} alt={this.props.title} />
+      <img onClick={this.handleFav} src={this.props.image_url} alt={this.props.title} />
       <p>{this.props.description}</p>
-      </article>
+      </article> */}
 
-    </>
+      </>
     )
   }
 }
