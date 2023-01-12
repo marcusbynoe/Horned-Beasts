5ff1a700b6ab96d2d4a7918c9a72a42a7c94a5ba
Marcus Bynoe <marcusbynoe57@gmail.com>
2023-01-12 15:42:24 -0700

    completed lab along in code review

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-
+import data from './Data/data.json';
+import SelectedBeast from './SelectedBeast';
 
 // 2nd Class Component
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     showModal: false,
-  //     selectedBeast: ''
-  //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      selectedBeast: {},
+      beastData: data,
+      title: '',
+      description: '',
+      horns: '',
+      image_url: '',
+    }
+  }
 
-  // handleOpenModal = (title) => {
-  //   this.setState({
-  //     showModal: true,
-  //     selectedBeast: title
-  //   });
-  // }
+  handleOpenModal = (beastObj) => {
+    this.setState({
+      showModal: true,
+      selectedBeast: beastObj,
+    });
+  }
 
-  // handleCloseModal = () => {
-  //   this.setState({
-  //     showModal: false,
-  //   });
-  // }
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  }
 
 
 
@@ -35,7 +41,13 @@ class App extends React.Component {
       <>
         <Header />
         <Main
-          // handleOpenModal={this.handleOpenModal}
+          beastData={this.state.beastData}
+          handleOpenModal={this.handleOpenModal}
+        />
+        <SelectedBeast
+          selectedBeast={this.state.selectedBeast}
+          openModal={this.state.showModal}
+          closeModal={this.handleCloseModal}
         />
         {/* <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
           <Modal.Header closeButton>{this.state.selectedBeast}</Modal.Header>
@@ -44,10 +56,11 @@ class App extends React.Component {
       </>
     )
   }
+
+
+
+
 }
-
-
-
 
 // 3rd Export the compnent 
 export default App;
